0596eeaeae524248144f11c366220c9d6f3fbbda
Ali Jahankhah <aral2@yahoo.com>
2025-09-26 10:59:46 +0100

    learn: getting input value and create an ascii draw with that

---
+++ week 1/coding/eyes.py
@@ -0,0 +1,13 @@
+# Getting a letter from the user
+print("\n Enter a character for the eyes: \n")
+letter = input()
+
+print(f"""
+    Beep's expression is now follows:
+      
+                #########
+                #  {letter} {letter}  #
+                #  ---  #
+                #########
+"""
+)
