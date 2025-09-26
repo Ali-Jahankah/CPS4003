4552712c9baca7812fd3202bb712a9bbed2a79e0
Ali Jahankhah <aral2@yahoo.com>
2025-09-26 10:47:34 +0100

    learn: variables and update values

---
+++ week 1/coding/variables.py
@@ -0,0 +1,34 @@
+# Display Beep's info
+beep_name = 'error test'
+battery_level = 100
+volume_level = 10
+light_level = 10
+
+
+
+
+
+print(f'''
+##########################
+# Diagnostics for Beep: {beep_name} #
+# ---------------------- #
+# Battery Level (%): {battery_level}  #
+# Volume Level (%):  {volume_level}  #
+# Light Level (%):   {light_level}  #
+##########################
+'''
+)
+
+# Adjust Beep's settings
+volume_level  = 50
+light_level =  30
+print(f'''
+##########################
+# Diagnostics for Beep: {beep_name} #
+# ---------------------- #
+# Battery Level (%): {battery_level}  #
+# Volume Level (%):  {volume_level}  #
+# Light Level (%):   {light_level}  #
+##########################
+'''
+)
