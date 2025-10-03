a8cc145a577249002afaa830db0d1784bd73eb76
Ali Jahankhah <aral2@yahoo.com>
2025-10-03 10:39:39 +0100

    use 'if' statements

---
+++ week-2/coding/if_statement.py
@@ -0,0 +1,6 @@
+genre = input("\n Please type the genre: Avanture or Romance => ").lower()
+if genre == 'advanture':
+    print('I like advanture books!')
+    print('I finished reading the book!')
+elif genre == 'romance':
+    print('I finished reading the book!')
