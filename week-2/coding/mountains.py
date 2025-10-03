6e29f4fd76ea287a47e1f6b47da1a16dfa51c50b
Ali Jahankhah <aral2@yahoo.com>
2025-10-03 11:22:58 +0100

    simple for loop

---
+++ week-2/coding/mountains.py
@@ -0,0 +1,18 @@
+# Suns using Loop
+print('\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n')
+suns =int(input("How many suns should I display?"))
+for count in range(0,suns,1):
+    print("""\n
+                |
+      `.        |        .'
+        `.    .---.    .'
+           .~       ~.
+          /   O   O   \\
+-- -- -- (             ) -- -- --
+          \    `-'    /
+           ~.       .~
+        .'    ~---~    `.
+      .'        |        `.
+                |
+                |
+         \n """)
