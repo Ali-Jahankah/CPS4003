7a35ebe501fe9a8e5063fc08c5ef6b64b886299d
Ali Jahankhah <aral2@yahoo.com>
2025-10-03 11:07:39 +0100

    simple loop in Python

---
+++ week-2/coding/simple_loop.py
@@ -0,0 +1,8 @@
+# Loops
+print('\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n')
+to_remove =int(input("How many cables should I remove?"))
+
+while to_remove>0:
+    print(f'cable removed! - {to_remove}')
+    to_remove-=1
+
