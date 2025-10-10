0ef3ef64787d6375011486d9b5418350eef929e3
Ali Jahankhah <aral2@yahoo.com>
2025-10-10 11:00:54 +0100

    learn: using modules in Python - random module

---
+++ week-3/coding/practice.py
@@ -0,0 +1,7 @@
+import random as rnd # OR from random import randrange
+from random import randrange as rand_range
+rand_num=rnd.random()
+print(rand_num)
+
+random_range = rand_range(6,100,1)
+print(random_range)
