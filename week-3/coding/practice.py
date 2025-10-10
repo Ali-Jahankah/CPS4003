67414218f76e497af3e44baeef9ca3f145f2605e
Ali Jahankhah <aral2@yahoo.com>
2025-10-10 11:54:17 +0100

    learn: use os for file system in Python

--- week-3/coding/practice.py
+++ week-3/coding/practice.py
@@ -1,7 +1,17 @@
 import random as rnd # OR from random import randrange
 from random import randrange as rand_range
+import os
 rand_num=rnd.random()
 print(rand_num)
 
 random_range = rand_range(6,100,1)
 print(random_range)
+
+# Learn how to use file system in Python
+path= os.getcwd()
+print(f"""
+      CWD==> {path}
+      """)
+
+for file in os.listdir(path):
+    print(file)
