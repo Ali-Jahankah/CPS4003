b8e8c01e43f092005502ad0d4b97570e47078e00
Ali Jahankhah <aral2@yahoo.com>
2025-10-17 10:39:17 +0100

    learn: simple Dict type

---
+++ week-4/coding/simple_dict.py
@@ -0,0 +1,11 @@
+def pattern():
+    sequences = {
+        "Short Sequence":3, "Medium Sequence":2, "Long SequSequence":1
+    }
+    return sequences
+
+def run():
+    print(pattern())
+
+if __name__ == "__main__":
+    run()
