7ef59d10cd38da357c271dec41e0fd8bf4bffe3b
Ali Jahankhah <aral2@yahoo.com>
2025-10-17 10:16:29 +0100

    learn data types: simple list

---
+++ week-4/coding/simple_list.py
@@ -0,0 +1,9 @@
+def getDirections():
+    directions=["Moving Forward", "Movung Backward", "Turn Left","Turn Right"]
+    return directions
+
+def run():
+    print(getDirections())
+    
+if __name__ == "__main__":
+    run()
