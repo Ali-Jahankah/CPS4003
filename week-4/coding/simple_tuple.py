bc8b5b5c088cf8a2d5627988b86d0cfe91289979
Ali Jahankhah <aral2@yahoo.com>
2025-10-17 10:28:41 +0100

    learn: simple Set and Tuple

---
+++ week-4/coding/simple_tuple.py
@@ -0,0 +1,9 @@
+def likelihood():
+    likelihoods=(50,38,27,99,4)
+    print(f"Minimum likelihood of falling: {min(likelihoods)}%")
+
+def run():
+    likelihood()
+
+if __name__ == "__main__":
+    run()
