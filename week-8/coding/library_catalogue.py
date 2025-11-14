259d67529e299cc6da2c61c59af6267f55522222
Ali Jahankhah <aral2@yahoo.com>
2025-11-14 16:13:50 +0000

    l,earn and practice class methods and attrs

---
+++ week-8/coding/library_catalogue.py
@@ -0,0 +1,29 @@
+class LibraryCatalogue:
+    library_name = "st Marys"
+    next_book_id = 2000
+    def __init__(self,title,author,genre,availability = True):
+        self.id = LibraryCatalogue.next_book_id
+        LibraryCatalogue.next_book_id +=1
+        self.title=title
+        self.author = author
+        self.genre=genre
+        self.availability = availability
+        
+    @classmethod
+    def set_library_name(cls,new_name):
+        cls.library_name = new_name
+    
+    def checkout(self):
+        self.availability = False
+        print(f"Book {self.title} checked-out!")
+    def return_book(self):
+        self.availability = True
+        print(f"Book {self.title} returned!")
+    def display_info(self):
+        print(f"Displaying book info: \n ID: {self.id} | Title: {self.title} | Author: {self.author} | Genre: {self.genre} | Availability: {'Available' if self.availability else 'Not available'}")
+        
+
+book1 = LibraryCatalogue(title="The Great Gatsby", author="F. Scott Fitzgerald", genre="Fiction")
+book2 = LibraryCatalogue(title="To Kill a Mockingbird", author="Harper Lee", genre="Classics")
+book1.display_info()
+book2.display_info()
